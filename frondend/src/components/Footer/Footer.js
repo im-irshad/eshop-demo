6dbfae1de3eb8a6c4b7f6a19e91b6c600fccffa0
Irshad Muhammad <imabbasi@msn.com>
2022-03-03 07:48:23 +0100

    front-end in progress

---
+++ frondend/src/components/Footer/Footer.js
@@ -0,0 +1,31 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+
+export default function Footer() {
+  return (
+    <Box
+      component="div"
+      sx={{ display: "flex", width: "100%", justifyContent: "space-between" }}
+    >
+      <Box
+        component="div"
+        sx={{ width: "30%", p: 2, border: "1px dashed grey" }}
+      >
+        <Button>Download Our App</Button>
+      </Box>
+      <Box
+        component="div"
+        sx={{ width: "30%", p: 2, border: "1px dashed grey" }}
+      >
+        <Button>E-Shop</Button>
+      </Box>
+      <Box
+        component="div"
+        sx={{ width: "30%", p: 2, border: "1px dashed grey" }}
+      >
+        <Button>Follow Us</Button>
+      </Box>
+    </Box>
+  );
+}
---
+++ frondend/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MidHeader from "./MidHeader";
+import TopHeader from "./TopHeader";
+
+function Header() {
+  return (
+    <>
+      <TopHeader />
+      <MidHeader />
+    </>
+  );
+}
+
+export default Header;
