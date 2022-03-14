5533d75947fd7edf75641355a80cedb7b7780dd3
Irshad Muhammad <imabbasi@msn.com>
2022-03-14 21:32:51 +0100

    save cart to LS

--- frondend/src/components/Cart/Cart.js
+++ frondend/src/components/Cart/Cart.js
@@ -1,7 +1,47 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 function Cart() {
-  return <div>Cart</div>;
+  const { cartItems } = useSelector((state) => state.cartReducer);
+  console.log(cartItems);
+  return (
+    <div>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Image</TableCell>
+              <TableCell align="right">Product</TableCell>
+              <TableCell align="right">Quantity</TableCell>
+              <TableCell align="right">Price</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {cartItems.map((item) => (
+              <TableRow
+                key={item._id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {item.name}
+                </TableCell>
+                <TableCell align="right">{item.name}</TableCell>
+                <TableCell align="right">{item.quantity}</TableCell>
+                <TableCell align="right">{item.price}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
 }
 
 export default Cart;
--- frondend/src/redux/actions/cartAction.js
+++ frondend/src/redux/actions/cartAction.js
@@ -6,15 +6,17 @@ export const addItToCart = (id, quantity) => async (dispatch, getState) => {
   const { data } = await axios.get(`/api/v1/product/${id}`);
   dispatch({
     type: ADD_TO_CART,
-    payload: data.product._id,
-    name: data.product.name,
-    price: data.product.price,
-    image: data.product.images[0].url,
-    stock: data.product.Stock,
-    quantity,
+    payload: {
+      product: data.product._id,
+      name: data.product.name,
+      price: data.product.price,
+      image: data.product.images[0].url,
+      stock: data.product.Stock,
+      quantity,
+    },
   });
 
-  //localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
 };
 
 export const rmvItFromCart = (id) => async (dispatch, getState) => {
@@ -23,5 +25,5 @@ export const rmvItFromCart = (id) => async (dispatch, getState) => {
     payload: id,
   });
 
-  //localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
 };
